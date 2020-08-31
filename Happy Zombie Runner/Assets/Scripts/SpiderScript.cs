4045960140a1a606d32dfc9ed744c4e6036c9e62
Airplane-cmd <69968263+Airplane-cmd@users.noreply.github.com>
2020-09-01 09:45:37 +1000

    SpiderAnimationUp1

--- Happy Zombie Runner/Assets/Scripts/SpiderScript.cs
+++ Happy Zombie Runner/Assets/Scripts/SpiderScript.cs
@@ -19,16 +19,50 @@ public class SpiderScript : MonoBehaviour
     public LayerMask Ground;
     public float BasicPositionX;
     public float BasicPositionY;
+    public float BasicRotationZ;
     //public float LeftFuckingTargetPositionX;
     //public float LeftFuckingTargetPositionY;
     //public float RightFuckingTargetPositionX;
     //public float RightFuckingTargetPositionY;
     //public float HeadFuckingTargetPositionX;
     //public float HeadFuckingTargetPositionY;
+    
+    
+
     private void Awake()
     {
         sprite = GetComponent<SpriteRenderer>(); 
         RD2D = GetComponent<Rigidbody2D>();
+    }
+    private void Move(bool LeftLeg, bool RightLeg, bool Head)
+    {
+        //float AngleV = 0.01f;
+        float distance = 0.9f;
+        //float Coaf;
+        if (LeftLeg == true && RightLeg == true && Head == false)
+        {
+            transform.Translate(new Vector2(HeadTarget.localPosition.x, HeadTarget.localPosition.y) * distance * Time.deltaTime);
+        }
+        else if (LeftLeg == false && RightLeg == true && Head == false)
+        {
+            transform.Translate(-0.5f,0,0);
+            transform.Rotate(0,0,90);
+            transform.Translate(new Vector2(HeadTarget.localPosition.x, HeadTarget.localPosition.y) * distance * Time.deltaTime);
+        }
+        else if (LeftLeg == true && RightLeg == true && Head == true)
+        {
+            transform.Rotate(0, 0, -90);
+            transform.Translate(0, -0.1f, 0);
+            transform.Translate(new Vector2(HeadTarget.localPosition.x, HeadTarget.localPosition.y) * distance * Time.deltaTime);
+        }
+        else if (LeftLeg == false && RightLeg == false && Head == false)
+        {
+            transform.Translate(new Vector2(HeadTarget.localPosition.x, HeadTarget.localPosition.y) * distance * Time.deltaTime);
+        }
+        if (LeftLeg == true && RightLeg == false && Head == false)
+        {
+            transform.Translate(new Vector2(HeadTarget.localPosition.x, HeadTarget.localPosition.y) * distance * Time.deltaTime);
+        }
     }
     private void FixedUpdate()
     {
@@ -40,11 +74,13 @@ public class SpiderScript : MonoBehaviour
         Debug.DrawRay(DownRightLeg.position, new Vector2(RightTarget.position.x - BasicPositionX, RightTarget.position.y - BasicPositionY) * RayDistance, Color.red);
         Debug.DrawRay(DownLeftLeg.position, new Vector2(LeftTarget.position.x - BasicPositionX, LeftTarget.position.y - BasicPositionY) * RayDistance, Color.red);
         Debug.DrawRay(Head.position, new Vector2(HeadTarget.position.x - BasicPositionX, HeadTarget.position.y - BasicPositionY) * RayDistance, Color.red);
-        if(DownRight==true)
-            Debug.Log("Ground2");
-        if (DownLeft == true)
-            Debug.Log("Ground1");
-        if (RightLeft == true)
-            Debug.Log("Forward");
+       //if(DownRight==true)
+        //Debug.Log("Ground2");
+        //if(DownLeft == true)
+        //Debug.Log("Ground1");
+        //if(RightLeft == true)
+        // Debug.Log("Forward");
+        Move(DownLeft, DownRight, RightLeft);
+        Debug.Log("Head target positionS:" + (HeadTarget.position.x - BasicPositionX, HeadTarget.position.y - BasicPositionY));
     }
-}
+ }
